3cd708f1c877802ab5eafb59c184e754cb7e951c
Yasmin Deodato <yasmin.deodato.beatriz@gmail.com>
2023-09-22 16:23:25 -0300

    versao otimizada openmp

--- versaoConcorrente/rainbowGameOfLifeOpenmp.c
+++ versaoConcorrente/rainbowGameOfLifeOpenmp.c
@@ -4,29 +4,34 @@
 #include <time.h>
 #include <omp.h>
 
-#define NTHREADS 8
+#define NTHREADS 4
+#define N 2048
+#define GERACOES 2000
 
-void printMatriz(float **matriz, int n) {
-    for( int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+float **matriz;
+float **copia;
+
+void printMatriz(float **matriz) {
+    for( int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
             printf("%.1f ", matriz[i][j]);
         }
         printf("\n");
     }
 }
 
-void zeraMatriz(float **matriz, int n) {
-    for( int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+void zeraMatriz(float **matriz) {
+    for( int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
             matriz[i][j] = 0.0;
         }
     }
 }
 
-int contaCelula(float **matriz, int x, int y, int n){
+int contaCelula(float **matriz, int x, int y){
     int contador = 0;
 
-    if (((x > 0) && (x < n-1)) && ((y > 0) && (y < n-1))) {
+    if (((x > 0) && (x < N-1)) && ((y > 0) && (y < N-1))) {
         for(int i = x-1 ; i <= x+1; i++){   
             for(int j = y-1; j <= y+1; j++){
                 if (matriz[i][j] > 0.0) contador += 1;
@@ -36,20 +41,20 @@ int contaCelula(float **matriz, int x, int y, int n){
         for(int i = x-1 ; i <= x+1; i++){   
             for(int j = y-1; j <= y+1; j++){
                 if ((i < 0) && (j < 0)) {
-                    if (matriz[n-1][n-1] > 0.0) contador += 1;
-                } else if((i < 0) && (j > n-1)){
-                    if (matriz[n-1][0] > 0.0) contador += 1;
-                } else if ((i > n-1) && (j > n-1)) {
+                    if (matriz[N-1][N-1] > 0.0) contador += 1;
+                } else if((i < 0) && (j > N-1)){
+                    if (matriz[N-1][0] > 0.0) contador += 1;
+                } else if ((i > N-1) && (j > N-1)) {
                     if (matriz[0][0] > 0.0) contador += 1;
-                } else if((i > n-1) && (j < 0)){
-                    if (matriz[0][n-1] > 0.0) contador += 1;
+                } else if((i > N-1) && (j < 0)){
+                    if (matriz[0][N-1] > 0.0) contador += 1;
                 } else if (i < 0){
-                    if (matriz[n-1][j] > 0.0) contador += 1;
+                    if (matriz[N-1][j] > 0.0) contador += 1;
                 } else if(j < 0){
-                    if (matriz[i][n-1] > 0.0) contador += 1;
-                } else if(i > n-1){
+                    if (matriz[i][N-1] > 0.0) contador += 1;
+                } else if(i > N-1){
                     if (matriz[0][j] > 0.0) contador += 1;
-                } else if(j > n-1){
+                } else if(j > N-1){
                     if (matriz[i][0] > 0.0) contador += 1;
                 } else {
                     if (matriz[i][j] > 0.0) contador += 1;
@@ -63,10 +68,10 @@ int contaCelula(float **matriz, int x, int y, int n){
     return contador;
 }
 
-float mediaCelula(float **matriz, int x, int y, int n){
+float mediaCelula(float **matriz, int x, int y){
     float contador = 0.0;
 
-    if (((x > 0) && (x < n-1)) && ((y > 0) && (y < n-1))) {
+    if (((x > 0) && (x < N-1)) && ((y > 0) && (y < N-1))) {
         for(int i = x-1 ; i <= x+1; i++){   
             for(int j = y-1; j <= y+1; j++){
                 contador += matriz[i][j];
@@ -76,20 +81,20 @@ float mediaCelula(float **matriz, int x, int y, int n){
         for(int i = x-1 ; i <= x+1; i++){   
             for(int j = y-1; j <= y+1; j++){
                 if ((i < 0) && (j < 0)) {
-                    contador += matriz[n-1][n-1];
-                } else if((i < 0) && (j > n-1)){
-                    contador += matriz[n-1][0];
-                } else if ((i > n-1) && (j > n-1)) {
+                    contador += matriz[N-1][N-1];
+                } else if((i < 0) && (j > N-1)){
+                    contador += matriz[N-1][0];
+                } else if ((i > N-1) && (j > N-1)) {
                     contador += matriz[0][0];
-                } else if((i > n-1) && (j < 0)){
-                    contador += matriz[0][n-1];
+                } else if((i > N-1) && (j < 0)){
+                    contador += matriz[0][N-1];
                 } else if (i < 0){
-                    contador += matriz[n-1][j];
+                    contador += matriz[N-1][j];
                 } else if(j < 0){
-                    contador += matriz[i][n-1];
-                } else if(i > n-1){
+                    contador += matriz[i][N-1];
+                } else if(i > N-1){
                     contador += matriz[0][j];
-                } else if(j > n-1){
+                } else if(j > N-1){
                     contador += matriz[i][0];
                 } else {
                     contador += matriz[i][j];
@@ -103,74 +108,29 @@ float mediaCelula(float **matriz, int x, int y, int n){
     return (contador/8.0);
 }
 
-void glider(float **matriz, int x, int y) { // x == 0 no glider e y == 0 no glider
-    /*// matriz[x][y] = 0.0;
-    matriz[x+1][y] = 1.0;
-    // matriz[x+2][y] = 0.0;
-    // matriz[x][y+1] = 0.0;
-    // matriz[x+1][y+1] = 0.0;
-    matriz[x+2][y+1] = 1.0;
-    matriz[x][y+2] = 1.0;
-    matriz[x+1][y+2] = 1.0;
-    matriz[x+2][y+2] = 1.0;*/
-
-    //Glinger girado
-    // matriz[x  ][y  ] = 0.0;
+void glider(float **matriz, int x, int y) { 
     matriz[x  ][y+1] = 1.0;
-    // matriz[x  ][y+2] = 0.0;
-    // matriz[x+1][y] = 0.0;
-    // matriz[x+1][y+1] = 0.0;
     matriz[x+1][y+2] = 1.0;
     matriz[x+2][y] = 1.0;
     matriz[x+2][y+1] = 1.0;
     matriz[x+2][y+2] = 1.0;
 }
 
-void rPentomino(float **matriz, int x, int y) { // x == 0 no rPentomino e y == 0 no rPentomino
-    // matriz[x  ][y  ] = 0.0;
+void rPentomino(float **matriz, int x, int y) {
     matriz[x+1][y  ] = 1.0;
-    // matriz[x+2][y  ] = 0.0;
     matriz[x  ][y+1] = 1.0;
     matriz[x+1][y+1] = 1.0;
     matriz[x+2][y+1] = 1.0;
     matriz[x  ][y+2] = 1.0;
-    // matriz[x+1][y+2] = 0.0;
-    // matriz[x+2][y+2] = 0.0;
 }
 
-void geracao(float **matriz, float **copia, int n) {
-    for(int i = 0 ; i <= n-1; i++){   
-        for(int j = 0; j <= n-1; j++){
-            int vivos = contaCelula(matriz, i, j, n);
-            // printf("%f - %d, %d - Vivos %d\n", matriz[i][j], i, j, vivos);
-
-
-            //qualquer celula morta com 3 (tres) vizinhos torna-se viva;
-            if ((matriz[i][j] == 0.0) && (vivos == 3)) {
-                copia[i][j] = mediaCelula(matriz, i, j, n);
-            
-            //qualquer celula viva com 2 (dois) ou 3 (tres) vizinhos deve sobreviver;
-            //seu valor permanece o mesmo ou reseta pra 1.0?
-            } else if ((matriz[i][j] > 0.0) && ((vivos == 2) || (vivos == 3))) {
-                copia[i][j] = 1.0;
-            } 
-
-            //qualquer outro caso, celulas vivas devem morrer e celulas ja mortas devem continuar mortas.
-            else {
-                copia[i][j] = 0.0;
-            }
-        }
-    }
-}
-
-float verificarNovoEstadoCelula(float **matriz, int n, int i, int j) {
-    int vivos = contaCelula(matriz, i, j, n);
+float verificarNovoEstadoCelula(float **matriz, int i, int j) {
+    int vivos = contaCelula(matriz, i, j);
     // printf("%f - %d, %d - Vivos %d\n", matriz[i][j], i, j, vivos);
-
 
     //qualquer celula morta com 3 (tres) vizinhos torna-se viva;
     if ((matriz[i][j] == 0.0) && (vivos == 3)) {
-        return mediaCelula(matriz, i, j, n);
+        return mediaCelula(matriz, i, j);
     //qualquer celula viva com 2 (dois) ou 3 (tres) vizinhos deve sobreviver;
     //seu valor permanece o mesmo ou reseta pra 1.0?
     } else if ((matriz[i][j] > 0.0) && ((vivos == 2) || (vivos == 3))) {
@@ -182,65 +142,38 @@ float verificarNovoEstadoCelula(float **matriz, int n, int i, int j) {
     }
 }
 
-void copiaMatrizes(float **matriz, float ** copia, int n) {
-    for(int i = 0 ; i <= n-1; i++){   
-        for(int j = 0; j <= n-1; j++){
+void copiaMatrizes(float **matriz, float ** copia) {
+    for(int i = 0 ; i <= N-1; i++){   
+        for(int j = 0; j <= N-1; j++){
             copia[i][j] = matriz[i][j];
         }
     }
 }
 
-int contaCelulasVivas(float **matriz, int n) {
-    int contador = 0;
-    for(int i = 0 ; i <= n-1; i++){   
-        for(int j = 0; j <= n-1; j++){
-            if(matriz[i][j] > 0.0) contador++;
-        }
-    }
-    return contador;
-}
-
-int interacoes(float **matriz, float nGeracoes, int n) {
-    float** copia = (float**)malloc(sizeof(float*)*n);
-    for (int i = 0; i < n; i++){
-        copia[i] = (float*)malloc(sizeof(float)*n);
-    }
-
-    int i=0, j=0;    
-    for(int g = 0 ; g < nGeracoes; g++){   
-        int vivos = 0;
+int interacoes(float **matriz) {
+    int i=0, j=0, vivos;    
+    for(int g = 0 ; g < GERACOES; g++){   
+        vivos = 0;
 
         omp_set_num_threads(NTHREADS);
         #pragma omp parallel private(i,j) reduction(+:vivos)
         #pragma omp for
-        for(i = 0 ; i <= n-1; i++){   
-            for(j = 0; j <= n-1; j++){
-                copia[i][j] = verificarNovoEstadoCelula(matriz, n, i, j);
-                //matriz[i][j] = copia[i][j];
+        for(i = 0 ; i <= N-1; i++){   
+            for(j = 0; j <= N-1; j++){
+                copia[i][j] = verificarNovoEstadoCelula(matriz, i, j);
 
                 if (copia[i][j] > 0.0) vivos++;
             }
         }
 
-        for(i = 0 ; i <= n-1; i++){   
-            for(j = 0; j <= n-1; j++){
-                matriz[i][j] = copia[i][j];
-            }
-        }
+        copiaMatrizes(copia, matriz);
 
-        printf("Geracao n %d - Celulas vivas: %d\n", g+1, vivos);
-        //copiaMatrizes(matriz, copia, n);
-        
-        //geracao(matriz, copia, n);
-
-        //copiaMatrizes(copia, matriz, n);
-
-        // printMatriz(matriz, n);
+        // printMatriz(matriz);
         // printf("____________________\n");
+        printf("Geracao n %d - Celulas vivas: %d\n", g+1, vivos);
     }
 
-    int contador = contaCelulasVivas(matriz, n);
-    return contador;
+    return vivos;
 }
 
 
@@ -249,18 +182,20 @@ void main() {
     double tempoTotalSegundos;
     int n = 2048;
 
-    float** matriz=(float**)malloc(sizeof(float*)*n);
+    matriz=(float**)malloc(sizeof(float*)*N);
+    copia = (float**)malloc(sizeof(float*)*N);
     for (int i = 0; i < n; i++){
-        matriz[i] = (float*)malloc(sizeof(float)*n);
+        matriz[i] = (float*)malloc(sizeof(float)*N);
+        copia[i] = (float*)malloc(sizeof(float)*N);
     }
-    
-    zeraMatriz(matriz, n);
 
+    // condicoes iniciais do tabuleiro
+    zeraMatriz(matriz);
     glider(matriz, 1, 1);
     rPentomino(matriz, 10, 30);
 
     tempoInicial = clock();
-    int resultado = interacoes(matriz, 2000, n);
+    int resultado = interacoes(matriz);
     tempoTotalSegundos = ((double)clock() - tempoInicial)/ CLOCKS_PER_SEC;
 
     //printMatriz(matriz, n);
